af3de4fe8b1708c48e8e2859e407013b19d7cada
Ketam <gui.c.sena@gmail.com>
2019-11-07 14:03:29 -0300

    Create a default handler for responses

--- functions/index.js
+++ functions/index.js
@@ -7,24 +7,23 @@ const app = dialogflow({ debug: true });
 const request = require('./connection/request');
 
 
-app.intent('Let me make a request', async (conv) => {
-    let input = "How is the weather going to be today?";
-    conv.ask("Surely! What do you want?");
-    data = await request.get(input);
-    conv.ask(JSON.stringify(data))
-    switch (data.code) {
-      case 0:
-        
-      break;
-      case 1:
-        //Intent2 function goes here
-      break;
-      case 2:
-        //Intent3 function goes here
-      break;
-      default:
-        conv.close("Sorry, i did not understand, could you repeat?")
-    }
+app.intent('Default Fallback Intent', async (conv) => {
+    conv.ask("Surely! Here it is:");
+    data = await request.get(conv.query);
+    conv.ask(data.response);
+    // switch (data.code) {
+    //   case 0:
+    //
+    //   break;
+    //   case 1:
+    //     //Intent2 function goes here
+    //   break;
+    //   case 2:
+    //     //Intent3 function goes here
+    //   break;
+    //   default:
+    //     conv.close("Sorry, i did not understand, could you repeat?")
+    // }
 });
 
 app.intent('Goodbye', conv => {
